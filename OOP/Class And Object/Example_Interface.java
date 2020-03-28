c7a65d08880fbea6d030a3f33f91162f86f75675
Mirza Arslan <60809539+Mirza57@users.noreply.github.com>
2020-03-28 21:07:52 +0500

    Add files via upload

---
+++ OOP/Class And Object/Example_Interface.java
@@ -0,0 +1,38 @@
+interface  Company
+  {
+    void name();
+    void salary();
+  }
+    class Employe1 implements Company
+	{
+		public void name(){
+			System.out.println(" My name is Arslan ");
+		}
+		
+		public void salary(){
+			System.out.println(" My current salary is 40,000PKR ");
+		}
+	}
+	
+	class Example_Interface implements Company
+	{
+		public void name(){
+			System.out.println(" My name is Faiq  ");
+		}
+		
+		public void salary(){
+			System.out.println(" My current salary is 50,500PKR ");
+		}
+		
+		public static void main(String args[]){
+			
+			Company c=new Employe1();
+			c.name();
+			c.salary();
+			
+			Company c1=new Example_Interface();
+			c1.name();
+			c1.salary();
+			
+	}
+	}
